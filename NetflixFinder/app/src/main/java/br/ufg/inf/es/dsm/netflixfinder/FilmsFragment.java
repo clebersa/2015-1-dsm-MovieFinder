c181329b598bc02af4907e697fe5978f75918f7f
clebersa <cleber.93cd@gmail.com>
2015-06-02 19:18:22 -0300

    Adicionando implementação inicial que contempla interface inicial e barra de pesquisa.

---
+++ NetflixFinder/app/src/main/java/br/ufg/inf/es/dsm/netflixfinder/FilmsFragment.java
@@ -0,0 +1,26 @@
+package br.ufg.inf.es.dsm.netflixfinder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cleber on 02/06/15.
+ */
+public class FilmsFragment extends Fragment {
+    public FilmsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_films, container, false);
+
+        //Connect list view
+
+        return rootView;
+    }
+}
