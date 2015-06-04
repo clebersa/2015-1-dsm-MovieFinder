a86069795b4e6e3bd45a4e933d1dbf072adb07e9
clebersa <cleber.93cd@gmail.com>
2015-06-04 13:25:59 -0300

    - Adicioando sleep no carregamento do aplicativo. - Adicioando logo do sistema no carregamento do aplicativo. - Corrigindo problema da referência do WebServiceConsumer.

--- NetflixFinder/app/src/main/java/br/ufg/inf/es/dsm/netflixfinder/activity/SplashActivity.java
+++ NetflixFinder/app/src/main/java/br/ufg/inf/es/dsm/netflixfinder/activity/SplashActivity.java
@@ -38,16 +38,6 @@ public class SplashActivity extends ActionBarActivity implements WebServiceConsu
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -58,6 +48,12 @@ public class SplashActivity extends ActionBarActivity implements WebServiceConsu
             ((FinderApplication) this.getBaseContext().getApplicationContext()).setConfiguration(configuration);
 
             Intent intent = new Intent(this, HomeActivity.class);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Log.e("SPLASH", "Error waiting to start home: " + e.getMessage());
+            }
+
             startActivity(intent);
             this.finish();
         }
